dae663395a68283cff42c7095809dcf58ccb124f
xubingyang <xby3957066@gmail.com>
2018-01-05 01:05:14 +0900

    fix a small ui problem.

--- showmore.js
+++ showmore.js
@@ -1,11 +1,15 @@
 function changeText(idElement) {
         let changetext = document.getElementById('changetext');
         let showmore = document.getElementById('showmore');
+        let basic = document.getElementById('basic');
+
         if (changetext.innerHTML === '更多') {
           changetext.innerHTML = '关闭';
           showmore.style.display = 'block';
+          basic.style.display = 'none';
         } else {
           changetext.innerHTML = '更多';
           showmore.style.display = 'none';
+          basic.style.display = 'block';
         }
       }
