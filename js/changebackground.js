f2c853d7693c8ce5dddddb22792fb3ec704c35a3
xubingyang <xby3957066@gmail.com>
2018-01-07 11:19:06 +0900

    able to change background image.

---
+++ js/changebackground.js
@@ -0,0 +1,13 @@
+main.addEventListener('click', (event) => {
+  event.preventDefault();
+  let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  let imageURL = '';
+  if (width > 768) {
+    imageURL = 'https://source.unsplash.com/random/1920x1080';
+  } else {
+    imageURL = 'https://source.unsplash.com/random/600x480';
+  }
+  fetch(imageURL).then((response) => {
+    document.body.style.backgroundImage = `url(${response.url})`;
+  });
+});
